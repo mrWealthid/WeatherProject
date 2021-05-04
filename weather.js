c447b6f1ff4a14659828156df7de2f3b99de19f1
Wealth Iduwe <wealthiduwe@gmail.com>
2021-05-04 20:08:10 +0100

    weather

---
+++ weather.js
@@ -0,0 +1,64 @@
+// Make An API Call using async await
+
+// Current Weather API
+const currentWeather =
+  "http://api.openweathermap.org/data/2.5/weather?q=lagos&appid=52e5cf2412ccca39bff6dde69153bb8c&units=imperial";
+const weather = async () => {
+  const current = await fetch(currentWeather);
+  const data = await current.json();
+  console.log(data);
+
+  document.getElementById(
+    "name"
+  ).innerHTML = ` ${data.name},${data.sys.country}`;
+  document.getElementById("desc").innerHTML = data.weather[0].description;
+  document.getElementById("temp").innerHTML = data.main.temp;
+  document.getElementById(
+    "humidity"
+  ).innerHTML = `Relative Humidity is ${data.main.humidity}`;
+};
+
+weather();
+
+const forecast =
+  "http://api.openweathermap.org/data/2.5/forecast?q=lagos&appid=52e5cf2412ccca39bff6dde69153bb8c&units=imperial";
+const weatherForecast = async () => {
+  const response = await fetch(forecast);
+  const data = await response.json();
+  console.log(data);
+
+  const dates = data.list[0].dt_txt;
+  const dateTrim = dates.substring(5, 11);
+  document.getElementById("r1c1").innerHTML = dateTrim;
+  document.getElementById("r1c3").innerHTML =
+    data.list[0].main.temp_min + "&deg;";
+  document.getElementById("r1c4").innerHTML =
+    data.list[0].main.temp_max + "&deg;";
+
+  const dates1 = data.list[1].dt_txt;
+  const dateTrim1 = dates1.substring(5, 11);
+  document.getElementById("r2c1").innerHTML = dateTrim1;
+  document.getElementById("r2c3").innerHTML =
+    data.list[1].main.temp_min + "&deg;";
+  document.getElementById("r2c4").innerHTML =
+    data.list[1].main.temp_max + "&deg;";
+
+  const dates2 = data.list[2].dt_txt;
+  const dateTrim2 = dates2.substring(5, 11);
+  document.getElementById("r3c1").innerHTML = dateTrim2;
+  document.getElementById("r3c3").innerHTML =
+    data.list[2].main.temp_min + "&deg;";
+  document.getElementById("r3c4").innerHTML =
+    data.list[2].main.temp_max + "&deg;";
+};
+
+weatherForecast();
+
+// function adds(arr, value) {
+//   const flow = arr.map(
+//     (element) => Object.keys(element) + ":" + (element.val + value)
+//   );
+//   return flow;
+// }
+
+// console.log(adds([{ val: 2 }, { val: 4 }, { val: 6 }], 4));
